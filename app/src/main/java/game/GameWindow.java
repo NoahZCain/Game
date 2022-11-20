479d11b5c48c7c88792a4decaf03f3961f07c1cb
Noah Cain <nzcain30@gmail.com>
2022-11-20 13:44:24 -0600

    Trying to create a game lol

---
+++ app/src/main/java/game/GameWindow.java
@@ -0,0 +1,18 @@
+package game;
+
+
+import javax.swing.*;
+
+public class GameWindow  {
+    private JFrame jFrame;
+
+    public GameWindow(){
+        jFrame = new JFrame();
+
+        jFrame.setSize(400,400);
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(3);
+
+
+    }
+}
